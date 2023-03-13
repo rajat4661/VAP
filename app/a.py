9b0f4eef09e16460f7239547d0c8ac14e019f677
rajat <rajat.c.4661@gmail.com>
2023-03-13 15:25:07 +0530

    base updated

---
+++ app/a.py
@@ -0,0 +1,23 @@
+# from flask import Flask, render_template, request
+# import os
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/login', methods=['POST'])
+# def login():
+#     username = request.form['username']
+#     email = request.form['email']
+    
+#     # Log the credentials to the terminal
+#     os.system(f"echo 'Username: {username}, Email: {email}'")
+    
+#     return render_template('success.html', username=username  , email=email)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+print("rajat")
+
